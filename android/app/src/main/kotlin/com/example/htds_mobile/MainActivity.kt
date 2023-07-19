aa018e2c091d37951aff1b5ec30343ba149677bc
TuTM <minhtu.it.haui@gmail.com>
2023-07-20 05:42:18 +0700

    go sign sdk android

--- android/app/src/main/kotlin/com/example/htds_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/htds_mobile/MainActivity.kt
@@ -1,7 +1,84 @@
 package com.example.htds_mobile
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import com.viettel.biometrics.signature.helpers.GoSignSDK
+import com.viettel.biometrics.signature.helpers.GoSignSDKSetup
+import com.viettel.biometrics.signature.listener.ServiceApiListener
+import com.viettel.biometrics.signature.listener.ServiceApiListenerEmpty
+import com.viettel.biometrics.signature.network.request.PendingAuthorisationRequest
+import com.viettel.biometrics.signature.network.response.CertificateResponse
+import com.viettel.biometrics.signature.network.response.ResponseError
+import com.viettel.biometrics.signature.ultils.BiometricApiType
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterFragmentActivity() {
+
+class MainActivity : FlutterFragmentActivity() {
+    private val CHANNEL = "go.sign.sdk.chanel"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GoSignSDKSetup.initialize(application, "https://remotesigning.viettel.vn:8773", "Xác thực khuôn mặt")
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            val token = call.argument<String>("token")
+            if (call.method == "registerDevice") {
+                GoSignSDK.get().registerDevice(
+                    this,
+                    token,
+                    BiometricApiType.AUTO,
+                    object : ServiceApiListener<CertificateResponse?> {
+                        override fun onSuccess(data: CertificateResponse?) {
+                            result.success(data)
+                        }
+
+                        override fun showLoading() {
+                        }
+
+                        override fun hideLoading() {
+                        }
+
+                        override fun onFail(error: ResponseError) {
+                            result.error(
+                                error.errorType.toString(),
+                                error.errorMessage,
+                                error.toString()
+                            )
+                        }
+                    })
+            }
+            if (call.method == "authorisationPendingRequest") {
+                val request: PendingAuthorisationRequest = PendingAuthorisationRequest()
+                request.request = call.argument("request")
+                request.hashAlgorithm = call.argument("hashAlgorithm")
+                request.transactionID = call.argument("transactionID")
+                GoSignSDK.get().authorisationPendingRequest(this,
+                    token, request, BiometricApiType.AUTO, object : ServiceApiListenerEmpty {
+                        override fun onSuccess() {
+                            result.success("success")
+                        }
+
+                        override fun showLoading() {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun hideLoading() {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun onFail(error: ResponseError) {
+                            result.error(
+                                error.errorType.toString(),
+                                error.errorMessage,
+                                error.toString()
+                            )
+                        }
+                    })
+            }
+        }
+    }
+
+
 }
---
+++ lib/go_sdk_chanel/go_sdk_chanel.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+const platform = MethodChannel('go.sign.sdk.chanel');
+
+class GoSdkChanel {
+  static Future registerDevice(String token) async {
+    final res = await platform.invokeMethod("registerDevice", {"token": token});
+    debugPrint(res);
+  }
+
+  static Future authorisationPendingRequest(String token, String request,
+      String hashAlgorithm, String transactionID) async {
+    final res = await platform
+        .invokeMethod("authorisationPendingRequest", {
+          "token": token,
+          "request": request,
+          "hashAlgorithm": hashAlgorithm,
+          "transactionID": transactionID,
+        });
+    debugPrint(res);
+  }
+}
